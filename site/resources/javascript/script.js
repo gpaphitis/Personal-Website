f7230fc2e88e95e2d6ce8c76798e42bf163a1d72
gpaphitis <giorgospaphitis@gmail.com>
2023-12-12 19:15:09 +0200

    Added navbar
    Added navbar links and menu button for links in small width device. About me and Contact currently point to index but CV points correctly to pdf

---
+++ site/resources/javascript/script.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("nav-menu").addEventListener("click", function () {
+    if (
+      document
+        .getElementById("nav-menu-options")
+        .classList.contains("menu-inactive")
+    ) {
+      document
+        .getElementById("nav-menu-options")
+        .classList.remove("menu-inactive");
+      document.getElementById("nav-menu-options").classList.add("menu-active");
+      document.getElementById("nav-container").classList.remove("height-fixed");
+      document.getElementById("nav-container").classList.add("height-auto");
+    } else {
+      document
+        .getElementById("nav-menu-options")
+        .classList.remove("menu-active");
+      document
+        .getElementById("nav-menu-options")
+        .classList.add("menu-inactive");
+      document.getElementById("nav-container").classList.remove("height-auto");
+      document.getElementById("nav-container").classList.add("height-fixed");
+    }
+  });
+});
