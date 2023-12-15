89dc529595aa89693f8fa63c65ae7d9a6580184f
gpaphitis <giorgospaphitis@gmail.com>
2023-12-15 16:02:26 +0200

    Added automatic calculation of height for sibling blocks
    Based on window width, the number of columns per block is calculated and they according blocks take the height of the row head by injecting css at head

--- site/resources/javascript/script.js
+++ site/resources/javascript/script.js
@@ -8,9 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
       document
         .getElementById("nav-menu-options")
         .classList.remove("menu-inactive");
-      document
-        .getElementById("nav-menu-options")
-        .classList.add("menu-active");
+      document.getElementById("nav-menu-options").classList.add("menu-active");
       document.getElementById("nav-container").classList.add("height-auto");
     } else {
       document
@@ -22,4 +20,28 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("nav-container").classList.remove("height-auto");
     }
   });
+  let docWidth = document.querySelector("body").offsetWidth;
+  console.log(docWidth);
+  if (docWidth >= 975) {
+    changeHeightSiblings(3);
+  } 
+  else if (docWidth >= 752) changeHeightSiblings(2);
 });
+function changeHeightSiblings(n) {
+  let heads = document.querySelectorAll(`.info-block.col-${n}-head`);
+  for (let i = 0; i < heads.length; i++) {
+    for (let j = 2; j <= n; j++) {
+      let height = `.info-block:nth-child(${i * n + j}) {height: ${
+        //0.5 is very important. We want it to be slightly bigger because if it is a bit smaller then next element
+        // will clip the edge of the previous one since they are floated i.e. _- next element clips at the underscore
+        heads[i].offsetHeight+0.5
+      }px;}`;
+      addStyle(height);
+    }
+  }
+}
+function addStyle(css) {
+  let el = document.createElement("style");
+  el.innerText = css;
+  document.head.appendChild(el);
+}
